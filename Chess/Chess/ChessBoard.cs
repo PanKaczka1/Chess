24c7d5bed56787f63684aa61be18182f58b5d6d3
PanKaczka <kapoludek@gmail.com>
2018-11-24 19:50:19 +0100

    meh

--- Chess/Chess/ChessBoard.cs
+++ Chess/Chess/ChessBoard.cs
@@ -42,12 +42,12 @@ namespace Chess
         {
             int i, j, k, l;
             for (i = 0; i < 8; i++) 
-                for (j = 0; j < 8; i++) 
+                for (j = 0; j < 8; j++) 
                 {
                     if (Board[i, j] is King && Board[i, j].Color == piece.Color) 
                     {
-                        for (k = 0; i < 8; i++)
-                            for (l = 0; j < 8; i++)
+                        for (k = 0; k < 8; k++)
+                            for (l = 0; l < 8; l++)
                             {
                                 if (Board[k, l].Color != piece.Color)
                                 {
